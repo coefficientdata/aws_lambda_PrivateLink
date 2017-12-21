1287737563f13b2c96525ddcd0af663b2a788f88
Adeel Ahmad <adeelahmad786@gmail.com>
2017-12-21 16:28:17 +0000

    added script

---
+++ CreatePrivateLink.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -- coding: utf-8 --
+
+"""
+File:           api.py
+Author:         Adeel Ahmad
+Email:          adeelahmad84@me.com
+Github:         https://github.com/adeelahmad84
+Description:    API script.
+"""
+
+from __future__ import print_function
+import boto3
+import os
+import sys
+
+aws_lambda = boto3.client('lambda')
+
+def main():
+    """
+    This module will liaise the Fresh Service Desk API.
+    """
+
+if __name__ == '__main__':
+    main()
