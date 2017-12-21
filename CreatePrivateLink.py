ee318392089457a4afae71aa5b149de51fe86d6b
Adeel Ahmad <adeelahmad786@gmail.com>
2017-12-21 21:40:57 +0000

    created response definition

--- CreatePrivateLink.py
+++ CreatePrivateLink.py
@@ -2,11 +2,10 @@
 # -- coding: utf-8 --
 
 """
-File:           api.py
+File:           CreatePrivateLink.py
 Author:         Adeel Ahmad
-Email:          adeelahmad84@me.com
 Github:         https://github.com/adeelahmad84
-Description:    API script.
+Description:    AWS Lambda to run aws-cli for PrivateLink
 """
 
 from __future__ import print_function
@@ -14,12 +13,64 @@ import boto3
 import os
 import sys
 
-aws_lambda = boto3.client('lambda')
+ec2 = boto3.client('ec2')
 
-def main():
-    """
-    This module will liaise the Fresh Service Desk API.
+def send_response(request, response, status=None, reason=None):
+    """ Send our response to the pre-signed URL supplied by CloudFormation
+    If no ResponseURL is found in the request, there is no place to send a
+    response. This may be the case if the supplied event was for testing.
     """
 
-if __name__ == '__main__':
-    main()
+    if status is not None:
+        response['Status'] = status
+
+    if reason is not None:
+        response['Reason'] = reason
+
+    if 'ResponseURL' in request and request['ResponseURL']:
+        url = urlparse.urlparse(request['ResponseURL'])
+        body = json.dumps(response)
+        https = httplib.HTTPSConnection(url.hostname)
+        https.request('PUT', url.path+'?'+url.query, body)
+
+    return response
+
+def handler(event, context):
+    """
+    Invoke aws-cli
+    """
+    response = {
+            'StackId': event['StackId'],
+            'RequestId': event['RequestId'],
+            'LogicalResourceId': event['LogicalResourceId'],
+            'Status': 'SUCCESS'
+            }
+
+    # PhysicalResourceId is meaningless here, but CloudFormation requires it
+    if 'PhysicalResourceId' in event:
+        response['PhysicalResourceId'] = event['PhysicalResourceId']
+    else:
+        response['PhysicalResourceId'] = str(uuid.uuid4())
+
+    # There is nothing to do for a delete request
+    if event['RequestType'] == 'Delete':
+        return send_response(event, response)
+
+    try:
+        privatelink = ec2.create_vpc_endpoint(
+                DryRun=event['ResourceProperties']['DryRun']
+                VpcEndpointType=event['ResourceProperties']['VpcEndpointType']
+                VpcId=event['ResourceProperties']['VpcId']
+                ServiceName=event['ResourceProperties']['ServiceName']
+                PolicyDocument=event['ResourceProperties']['PolicyDocument']
+                SubnetIds=event['ResourceProperties']['SubnetIds']
+                ClientToken=event['ResourceProperties']['ClientToken']
+                PrivateDnsEnabled=event['ResourceProperties']['PrivateDnsEnabled']
+                )
+
+        response['Data'] = {
+                
+                }
+
+
+
